0a7a7a50b90d8ea70fba179a665901f336992efd
highfreshness <meteo0718@gmail.com>
2024-03-25 14:47:54 +0900

    🔥 Remove FastAPI in Celery task

--- app/main.py
+++ app/main.py
@@ -1,15 +1,45 @@
+import os
 import uvicorn
 from fastapi import FastAPI
-from tasks import add
+from fastapi.concurrency import run_in_threadpool
+from celery import signature, Celery
+from celery.result import AsyncResult
 
 app = FastAPI()
 
+BROKER_URL = os.getenv("BROKER_URL", "redis://localhost")
+BACKEND_URL = os.getenv("BACKEND_URL", "redis://localhost")
 
-@app.get("/inference")
-def inference(a: int, b: int) -> dict:
-    result = add.delay(a, b)  # Redis로 전송
-    answer = result.get(timeout=30)  # 10초 동안 작업의 완료를 기다림
-    return {"answer": answer}
+celery_app = Celery("tasks", broker=BROKER_URL, backend=BACKEND_URL)
+
+
+@app.get("/inference/id")
+def inference_id(a: int, b: int) -> dict:
+    add_signature = signature("add", args=(a, b), app=celery_app)
+    task: AsyncResult = add_signature.delay()
+    # 요청 직후 답변 테스트
+    # task_result = AsyncResult(task.id, app=celery_app)
+    # while True:
+    #     if task.state == "SUCCESS":
+    #         break
+    # return {"Task_ID": task.get()}
+    return {"Task_ID": task.id}
+
+
+@app.get("/inference/result")
+def inference_result(id: str) -> dict:
+    task_result = AsyncResult(id, app=celery_app)
+    # Task의 상태 확인
+    if task_result.state == "PENDING":
+        # Task가 아직 완료되지 않았습니다.
+        return {"message": "Task is still processing..."}
+    elif task_result.state == "SUCCESS":
+        # Task가 성공적으로 완료되었습니다. 결과를 가져옵니다.
+        result = task_result.get()
+        return {"Result": result}
+    else:
+        # Task가 실패하거나 다른 상태일 경우
+        print("Task state:", task_result.state)
 
 
 if __name__ == "__main__":
--- celery_worker/tasks.py
+++ celery_worker/tasks.py
@@ -5,12 +5,12 @@ from time import sleep
 BROKER_URL = os.getenv("BROKER_URL", "redis://localhost")
 BACKEND_URL = os.getenv("BACKEND_URL", "redis://localhost")
 
-app = Celery("tasks", broker=BROKER_URL, backend=BACKEND_URL)
+celery_app = Celery("tasks", broker=BROKER_URL, backend=BACKEND_URL)
 
 TIME = os.getenv("SLEEP_TIME", "5")
 
 
-@app.task
+@celery_app.task(name="add")
 def add(a: int, b: int) -> int:
     answer = a + b
     sleep(int(TIME))
